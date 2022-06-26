54e175b3e586b55bc2a94b147b61cc5668760d28
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2022-06-25 21:40:33 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ if(isset($_POST['submit'])){
     $subject = $_POST['subject'];
     $message = $_POST['message'];
 
-    $mailTo ="1136919478@qq.com";
+    $mailTo ="xuwillie1@gmail.com";
     $headers = "From: ".$mailFrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
     
